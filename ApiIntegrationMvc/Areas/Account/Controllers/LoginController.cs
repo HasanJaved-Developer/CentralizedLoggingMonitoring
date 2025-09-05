2277534a4440f4f3d23c251fb652f3e6d8773025
Hasan Javed <hasanjaved.dev@gmail.com>
2025-09-05 16:08:21 +0500

    Login view, controller model, login layout, client side and server side validations added in the web app.

---
+++ ApiIntegrationMvc/Areas/Account/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using ApiIntegrationMvc.Areas.Account.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIntegrationMvc.Areas.Account.Controllers
+{
+    [Area("Account")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+        public IActionResult Index()
+        {
+            ViewBag.Error = TempData["Error"];     // one-time error
+            return View(new LoginViewModel());     // empty fields
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+                        
+            if(model.Username == "admin" && model.Password == "123")
+                return RedirectToAction("Index", "Home");     // PRG on success too            
+            
+            TempData["Error"] = "Invalid username or password.";
+            return RedirectToAction(nameof(Index));   // ← PRG on failure
+            
+        }
+    }
+}
